7f91c3bb79109ae4c45943631a805271392f3516
pearlslugs <pearlslugs@gmail.com>
2022-07-07 13:03:34 -0600

    source

---
+++ Source/PointlessFighting/PointlessFightingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PointlessFightingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POINTLESSFIGHTING_API APointlessFightingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
